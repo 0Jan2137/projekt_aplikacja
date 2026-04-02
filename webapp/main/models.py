f304548ec4742c0a523dc51a2c7f3497352da0e4
JohnnysEmporium <sobczak.yzf@gmail.com>
2026-04-02 13:28:06 +0200

    Integrated loose-frontend with Django. Implemented database models, adapted frontend to use database values. Created migrations for database with initial data. Integrated existing user authentication with new app. Removed unnecessary modules.

--- webapp/main/models.py
+++ webapp/main/models.py
@@ -1,3 +1,34 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
+
+
+class Transaction(models.Model):
+	CATEGORY_FOOD = 'food'
+	CATEGORY_TRANSPORT = 'transport'
+	CATEGORY_GROCERIES = 'groceries'
+	CATEGORY_BILLS = 'bills'
+	CATEGORY_ENTERTAINMENT = 'entertainment'
+
+	CATEGORY_CHOICES = [
+		(CATEGORY_FOOD, 'Food'),
+		(CATEGORY_TRANSPORT, 'Transport'),
+		(CATEGORY_GROCERIES, 'Groceries'),
+		(CATEGORY_BILLS, 'Bills'),
+		(CATEGORY_ENTERTAINMENT, 'Entertainment'),
+	]
+
+	amount = models.DecimalField(max_digits=10, decimal_places=2)
+	category = models.CharField(max_length=32, choices=CATEGORY_CHOICES)
+	description = models.CharField(max_length=255, blank=True)
+	date = models.DateField()
+	user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='transactions')
+	created_at = models.DateTimeField(auto_now_add=True)
+
+	@property
+	def category_name(self):
+		return self.get_category_display()
+
+	def __str__(self):
+		return f'{self.get_category_display()} - {self.amount} PLN ({self.date})'
